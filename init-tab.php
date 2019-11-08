342eec74a09f630c26ad4576619d6af9d411a2e3
Anthony Bac <anthonybac@MacBook-Pro-de-admin-2.local>
2019-11-08 10:49:16 +0100

    have to fix collision

--- init-tab.php
+++ init-tab.php
@@ -39,16 +39,40 @@
 	function place_boat($loop, $player_board, $basexyd) {
 		if ($loop == "line") {
 			while ($basexyd[3] >= $basexyd[1]) {
-					$player_board[$basexyd[0]][$basexyd[1]] = "B";
-					if ($basexyd[1] <= 9)
-						$basexyd[1]++;
+				$player_board[$basexyd[0]][$basexyd[1]] = "B";
+				if ($basexyd[0] - 1 >= 0)
+					if ($player_board[$basexyd[0] - 1][$basexyd[1]] == "~")
+						$player_board[$basexyd[0] - 1][$basexyd[1]] = "C";
+				if ($basexyd[0] + 1 <= 9)
+					if ($player_board[$basexyd[0] + 1][$basexyd[1]] == "~")
+						$player_board[$basexyd[0] + 1][$basexyd[1]] = "C";
+				if ($basexyd[1] - 1 >= 0)
+					if ($player_board[$basexyd[0]][$basexyd[1] - 1] == "~")
+						$player_board[$basexyd[0]][$basexyd[1] - 1] = "C";
+				if ($basexyd[1] + 1 <= 9)
+					if ($player_board[$basexyd[0]][$basexyd[1] + 1] == "~")
+						$player_board[$basexyd[0]][$basexyd[1] + 1] = "C";
+				if ($basexyd[1] <= 9)
+					$basexyd[1]++;
 			}
 		}
 		else
 			while ($basexyd[2] >= $basexyd[0]) {
-					$player_board[$basexyd[0]][$basexyd[1]] = "B";
-					if ($basexyd[0] <= 9)
-						$basexyd[0]++;
+				$player_board[$basexyd[0]][$basexyd[1]] = "B";
+				if ($basexyd[0] - 1 >= 0)
+					if ($player_board[$basexyd[0] - 1][$basexyd[1]] == "~")
+						$player_board[$basexyd[0] - 1][$basexyd[1]] = "C";
+				if ($basexyd[0] + 1 <= 9)
+					if ($player_board[$basexyd[0] + 1][$basexyd[1]] == "~")
+						$player_board[$basexyd[0] + 1][$basexyd[1]] = "C";
+				if ($basexyd[1] - 1 >= 0)
+					if ($player_board[$basexyd[0]][$basexyd[1] - 1] == "~")
+						$player_board[$basexyd[0]][$basexyd[1] - 1] = "C";
+				if ($basexyd[1] + 1 <= 9)
+					if ($player_board[$basexyd[0]][$basexyd[1] + 1] == "~")
+						$player_board[$basexyd[0]][$basexyd[1] + 1] = "C";
+				if ($basexyd[0] <= 9)
+					$basexyd[0]++;
 			}
 		return $player_board;
 	}
@@ -56,51 +80,38 @@
 	//Vérifier si le bateau peut s'installer (en colonne)
 	function check_co_boat_col($loop, $player_board, $xyd) {
 		while ($xyd[2] >= $xyd[0]) {
-			if ($player_board[$xyd[0]][$xyd[1]] != "~")
-				break;
-			if ($xyd[0] < 9)
-				$xyd[0]++;
-			else
-				break;
+			if ($player_board[$xyd[0]][$xyd[1]] != "~") {
+				println("Votre bateau est en collision ou dans le territoire d'un autre !");
+				return $loop = true;
+			}
+			$xyd[0]++;
 		}
-		if ($xyd[0] >= $xyd[2])
-			return $loop = "col";
-		else {
-			println("Votre bateau est en collision ou dans le territoire d'un autre !");
-			return $loop = true;
-		}
+		return $loop = "col";
 	}
 
 	//Vérifier si le bateau peut s'installer (en ligne)
 	function check_co_boat_line($loop, $player_board, $xyd) {
 		while ($xyd[3] >= $xyd[1]) {
-			if ($player_board[$xyd[2]][$xyd[3]] != "~")
-				break;
-			if ($xyd[1] < 9)
-				$xyd[1]++;
-			else
-				break;
+			if ($player_board[$xyd[2]][$xyd[3]] != "~") {
+				println("Votre bateau est en collision ou dans le territoire d'un autre !");
+				return $loop = true;
+			}
+			$xyd[1]++;
 		}
-		if ($xyd[1] >= $xyd[3])
-			return $loop = "line";
-		else {
-			println("Votre bateau est en collision ou dans le territoire d'un autre !");
-			return $loop = true;
-		}
+		return $loop = "line";
 	}
 
 	//Entre les coordonnées du bateau
 	function enter_co_boat($xyd, $player_board) {
 		$nbboat = 5;
 		while ($nbboat > 0) {
-			println("Il vous reste $nbboat restants.");
+			println("Il vous reste $nbboat bateaux restants.");
 			$loop = "true";
 			while ($loop == "true") {
 				$xyd = trim(fgets(STDIN));
 				$xyd = str_replace(" ", "", $xyd);
 				$xyd = strtoupper($xyd);
 				$xyd = str_split($xyd);
-				println("J'AI ENTRE " . count($xyd) . " CARACTERES");
 				//ADAPTATION DE NOMBRE A 2 CASES
 				if (count($xyd) == 6) {
 					if ($xyd[1] == "1" && $xyd[2] == "0" && $xyd[4] == "1" && $xyd[5] == "0") {
@@ -122,7 +133,6 @@
 					}
 					unset($xyd[4]);
 				}
-				print_r($xyd);
 				$xyd[0] = (ord($xyd[0]) - 65);
 				$xyd[1]--;
 				if ($xyd[1] == "10")
@@ -132,7 +142,6 @@
 				if ($xyd[3] == "10")
 					$xyd[3] = 9;
 				//VERIFICATION
-				print_r($xyd);
 				if ($xyd[0] >= "0" && $xyd[0] <= "9" && $xyd[1] >= "0" && $xyd[1] <= "9" && $xyd[2] <= "9" && $xyd[3] <= "9") {
 					$basexyd = $xyd;
 					if ($xyd[2] > $xyd[0] && $xyd[3] == $xyd[1])
